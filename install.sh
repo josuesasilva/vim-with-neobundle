21a1c61ebde7850745441528c42f61a5979d0584
Josué <josuesasilva@gmail.com>
2014-02-09 14:44:12 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/josuesasilva/vim-with-neobundle.git ~/.vim
 ln -sf ~/.vim/.vimrc ~/.vimrc
 mkdir -p ~/.fonts
-cp -R ~/.vim/font/* ~/.fonts
+cp -R ~/.vim/fonts/* ~/.fonts
 fc-cache -vf
+vim
